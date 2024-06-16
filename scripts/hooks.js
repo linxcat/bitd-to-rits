97fddf74f1cc2cdb170cce58e493ac4e9202e997
Rio Silver <rjcsilver@gmail.com>
2024-06-15 20:26:28 -0400

    Swapped hook to add Assense/Emergence to actors for one that adds to Actor

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,32 +1,52 @@
 export async function registerHooks() {
   Hooks.once("ready", () => {
+    Hooks.on("ready", () => {
+      game?.system?.model?.Actor?.character?.attributes?.magic?.updateSource({
+        "exp": 0,
+        "exp_max": 0,
+        "label": "BITD.SkillsMagic",
+        "skills": {
+           'assense': {
+              "label": "BITD.SkillsAssense",
+              "value": 0,
+              "max": 4
+           },
+           'emergence': {
+              "label": "BITD.SkillsEmergence",
+              "value": 0,
+              "max": 4
+           }
+        }
+      })
+    });
+
     Hooks.on('preCreateActor', (actor, data, options, user) => {
       if (actor.type === 'character') actor.updateSource({'system.experience_max': 9})
     });
 
-    Hooks.on('preCreateActor', (actor, data, options, user) => {
-      if (actor.type === 'character') {
-         actor.updateSource({
-            'system.attributes.magic': {
-               "exp": 0,
-               "exp_max": 0,
-               "label": "BITD.SkillsMagic",
-               "skills": {
-                  'assense': {
-                     "label": "BITD.SkillsAssense",
-                     "value": 0,
-                     "max": 4
-                  },
-                  'emergence': {
-                     "label": "BITD.SkillsEmergence",
-                     "value": 0,
-                     "max": 4
-                  }
-               }
-            }
-         });
-      }
-    });
+    // Hooks.on('preCreateActor', (actor, data, options, user) => {
+    //   if (actor.type === 'character') {
+    //      actor.updateSource({
+    //         'system.attributes.magic': {
+    //            "exp": 0,
+    //            "exp_max": 0,
+    //            "label": "BITD.SkillsMagic",
+    //            "skills": {
+    //               'assense': {
+    //                  "label": "BITD.SkillsAssense",
+    //                  "value": 0,
+    //                  "max": 4
+    //               },
+    //               'emergence': {
+    //                  "label": "BITD.SkillsEmergence",
+    //                  "value": 0,
+    //                  "max": 4
+    //               }
+    //            }
+    //         }
+    //      });
+    //   }
+    // });
 
     Hooks.on('renderActor', (app, [html], data) => {
       const actor                  = app.document;
