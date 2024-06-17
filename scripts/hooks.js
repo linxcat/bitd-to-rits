b258856ba2a9e5d1945d5cf53d3e3e1d1fe31bfa
linxcat <cone.lavoy@gmail.com>
2024-06-17 16:48:10 -0400

    Ready to merge

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -24,30 +24,6 @@ export async function registerHooks() {
       if (actor.type === 'character') actor.updateSource({'system.experience_max': 9})
     });
 
-    // Hooks.on('preCreateActor', (actor, data, options, user) => {
-    //   if (actor.type === 'character') {
-    //      actor.updateSource({
-    //         'system.attributes.magic': {
-    //            "exp": 0,
-    //            "exp_max": 0,
-    //            "label": "BITD.SkillsMagic",
-    //            "skills": {
-    //               'assense': {
-    //                  "label": "BITD.SkillsAssense",
-    //                  "value": 0,
-    //                  "max": 4
-    //               },
-    //               'emergence': {
-    //                  "label": "BITD.SkillsEmergence",
-    //                  "value": 0,
-    //                  "max": 4
-    //               }
-    //            }
-    //         }
-    //      });
-    //   }
-    // });
-
     Hooks.on('renderActor', (app, [html], data) => {
       const actor                  = app.document;
       var   awakened               = actor.system.awakened
